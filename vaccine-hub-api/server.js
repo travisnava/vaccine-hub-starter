510e0a5b08752a3191d4b8458842a1ee3f3594d3
travisnava <trn5106@psu.edu>
2022-06-28 13:34:33 -0400

    Set up environment variables and config for vaccine lab 2, moved files into api folder

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const { PORT } = require("./config")
 
 
 const { NotFoundError } = require("./utils/errors")
@@ -28,8 +29,6 @@ app.use((err, req, res, next) => {
         error: {message, status},
     })
 })
-
-const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running http://localhost:${PORT}`)
---
+++ vaccine-hub-api/vaccine-hub-schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id          SERIAL PRIMARY KEY,
+    password    TEXT NOT NULL,
+    first_name  TEXT NOT NULL,
+    last_name   TEXT NOT NULL,
+    email       TEXT NOT NULL UNIQUE CHECK(POSITION('@' IN email) > 1),
+    location    TEXT NOT NULL,
+    date        DATE NOT NULL DEFAULT NOW()
+);
